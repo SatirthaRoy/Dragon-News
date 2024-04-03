0b494a97ba77310788abab7029455434fbff25c3
himu <satirtharoy2003@gmail.com>
2024-04-03 20:51:02 +0600

    project started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Outlet} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div className="lg:w-11/12 mx-auto">
+      <Outlet/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="font-poppins text-4xl text-green-500">Hellow mother fucker</h1>
+      <h1 className="text-4xl text-green-500">Hellow mother fucker</h1>
+    </div>
+  )
+}
+
+export default Home
